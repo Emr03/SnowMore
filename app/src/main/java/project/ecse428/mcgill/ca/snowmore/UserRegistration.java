d9c4e65b1149021368c6672f18a6affbc7369ed4
Elsa Riachi <elsa.riachi@mail.mcgill.ca>
2018-02-20 17:44:44 -0500

    Fixed Registration (hopefully, I don't know what happened)

--- app/src/main/java/project/ecse428/mcgill/ca/snowmore/UserRegistration.java
+++ app/src/main/java/project/ecse428/mcgill/ca/snowmore/UserRegistration.java
@@ -21,11 +21,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -50,6 +51,7 @@ public class UserRegistration extends AppCompatActivity {
     private Button registration_button;
     private Button signin_button;
     private User user;
+    private FirebaseUser fb_user;
     private Dialog dialog = null;
     private Context context = null;
 
@@ -59,15 +61,23 @@ public class UserRegistration extends AppCompatActivity {
     private FirebaseDatabase myFirebaseDatabase;
     private DatabaseReference myRef;
     private String userID;
+    private static boolean success_flag = false;
+    private ProgressBar progbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
 
+        // get an Authentication instance, handles authentication
         mAuth = FirebaseAuth.getInstance();
+
+        // get a database instance
         myFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        // get a reference to the database
         myRef = myFirebaseDatabase.getReference();
+
         //FirebaseUser user = mAuth.getCurrentUser();
         //userID = user.getUid();
 
@@ -75,7 +85,18 @@ public class UserRegistration extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle("Snow More");
         context = UserRegistration.this;
+
+        progbar = new ProgressBar(this);
+
         setUpVariables();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        fb_user = mAuth.getCurrentUser();
+
     }
 
     @Override
@@ -122,10 +143,13 @@ public class UserRegistration extends AppCompatActivity {
 
     //Registration button action
     public void registerButton(View view) {
+
+        // if email field is empty, show error message
         if(TextUtils.isEmpty(email.getText().toString())) {
             error_message_email.setText("Please enter email");
             error_message_email.setVisibility(View.VISIBLE);
         }
+        // check if email form is valid
         else {
             if(!user.check_email(email.getText().toString())) {
                 error_message_email.setText("Invalid email");
@@ -177,39 +201,53 @@ public class UserRegistration extends AppCompatActivity {
         startActivity(signin);
     }
 
+//    private void addNewUser() {
+//        //results in permission denied!
+//        myRef.child("new item").setValue("something");
+//        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+//                .addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+//
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithEmail:success");
+//                            UserRegistration.success_flag = true;
+//                    }
+//                });
+
     private void addNewUser() {
         //results in permission denied!
         myRef.child("new item").setValue("something");
         mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
+
+                        if(task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
+                            UserRegistration.success_flag = true;
+                            Intent signin = new Intent(context , Login.class);
+                            startActivity(signin);
 
-//                            User user = new User(fullname.getText().toString(), email.getText().toString());
-//                            DatabaseReference userRef = myRef.child("Users");
-//                                    //.child(userID);
-//                            userRef.child("Name").setValue(user.getName());
-                        } else {
 
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            User user = new User(fullname.getText().toString(), email.getText().toString());
+                            FirebaseUser fb_user = mAuth.getCurrentUser();
+                            userID = fb_user.getUid();
+                            DatabaseReference userRef = myRef.child("Users").child(userID);
+                            Map<String, Object> dataMap = new HashMap<String, Object>();
+                            dataMap.put("user_info", user.toMap());
+                            userRef.updateChildren(dataMap);
+                        }
+                        else{
+                            Log.d(TAG, "signInWithEmail:failure");
+                            UserRegistration.success_flag = false;
                             Toast.makeText(UserRegistration.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
                         }
                     }
                 });
-
-        User user = new User(fullname.getText().toString(), email.getText().toString());
-        FirebaseUser fb_user = mAuth.getCurrentUser();
-        userID = fb_user.getUid();
-        DatabaseReference userRef = myRef.child("Users").child(userID);
-        Map<String, Object> dataMap = new HashMap<String, Object>();
-        dataMap.put("user_info", user.toMap());
-        userRef.updateChildren(dataMap);
     }
 
     public void createDialog() {
@@ -220,8 +258,7 @@ public class UserRegistration extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         addNewUser();
-                        Intent signin = new Intent(context , Login.class);
-                        startActivity(signin);
+
                     }
                 });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
